31447df058d7ea525555ce9c48a3f66d415c3893
BIGfoot496 <elijah.beregovsky@gmail.com>
2026-09-07 18:41:01 +0300

    Built site. Let's see if it works

---
+++ docs/_site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,7 @@
+var store = [{
+        "title": "The Schrödinger Two-Electron Atomic Problem",
+        "excerpt":"A paper a day: day 1 Today’s paper is about the issues that arose when the old quantum theory met with the three body problem that is a helium atom, and the ways it was ultimately solved. This is not one of the original 1928-1929 Hylleraas papers – those ones...","categories": ["A paper a day"],
+        "tags": ["quantum mechanics"],
+        "url": "/a%20paper%20a%20day/2026/09/06/the-shrodinger-two-electron-atomic-problem.html",
+        "teaser": null
+      }]
